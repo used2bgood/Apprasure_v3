6501ae100bc0b15a2c8382f565e7df78c4dc1063
used2bgood <jarenbanks@gmail.com>
2025-08-08 21:10:40 -0500

    initial Eleventy site

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk"
+  };
+};
