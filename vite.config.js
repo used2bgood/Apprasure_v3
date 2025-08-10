ea6779e97d3cbb3460a49412e83d9745589abf61
used2bgood <jarenbanks@gmail.com>
2025-08-09 20:21:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-  build: { outDir: 'dist' }
-})
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-}) 
+  build: { outDir: 'dist' },
+})
