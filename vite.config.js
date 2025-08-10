3bf7d98af6cae8651d722fb4d15ab9d2ede360e9
used2bgood <jarenbanks@gmail.com>
2025-08-09 20:18:01 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true
-  },
+  base: '/',
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  build: { outDir: 'dist' }
+})
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
